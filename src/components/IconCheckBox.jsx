f2d00cc242ad0011a6f3042f971e3dc28b724631
johnnyleek <leek1j@cmich.edu>
2021-08-09 21:17:50 -0400

    Started Project Page

---
+++ src/components/IconCheckBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/components/IconCheckBox.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function IconCheckBox({ htmlFor, icon, label, checkFunc, checked }) {
+    return (
+        <>
+        <input type="checkbox" name={htmlFor} id={htmlFor} className="checkbox" onChange={() => checkFunc(htmlFor)} checked={checked(htmlFor)}/>
+        <div className={`box-container shadow`}>
+            <label htmlFor={htmlFor} className="label"><FontAwesomeIcon icon={icon} size={'3x'}/></label>
+            <label htmlFor={htmlFor} className="label">{label}</label>
+        </div>
+        </>
+    )
+}
+
+IconCheckBox.propTypes = {
+    htmlFor: PropTypes.string,
+    icon: PropTypes.any,
+    label: PropTypes.string,
+    checkFunc: PropTypes.func,
+    checked: PropTypes.bool
+}
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,6 +1,11 @@
 import React, { useContext, useEffect } from 'react';
 import { TransitionContext } from '../components/TransitionScreen';
 import BackButton from '../components/BackButton';
+import IconCheckBox from '../components/IconCheckBox';
+import {
+    faReact,
+    faJsSquare
+} from '@fortawesome/free-brands-svg-icons';
 
 export default function ProjectsPage() {
     const { onLoad } = useContext(TransitionContext);
@@ -11,6 +16,9 @@ export default function ProjectsPage() {
         <div id="projects">
             <BackButton />
             <h1 className="title">Projects</h1>
+
+            <IconCheckBox htmlFor="react" icon={faReact} label="React.JS" checkFunc={() => console.log('react checked')} checked={() => { return true }} />
+            <IconCheckBox htmlFor="js" icon={faJsSquare} label="JavaScript" checkFunc={() => console.log('js checked')} checked={() => { return false }} />
         </div>
     );
 }
